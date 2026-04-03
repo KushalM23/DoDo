879bb99025d0ff198c74e90f861d142493b00efb
Kushal <kushalll.study@gmail.com>
2026-04-03 18:32:09 +0530

    fix deployment issues

--- dodoweb/next.config.mjs
+++ dodoweb/next.config.mjs
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  distDir: process.env.NODE_ENV === "production" ? ".next-build" : ".next",
   env: {
     NEXT_PUBLIC_API_BASE_URL:
       process.env.NEXT_PUBLIC_API_BASE_URL || process.env.API_BASE_URL || "",
